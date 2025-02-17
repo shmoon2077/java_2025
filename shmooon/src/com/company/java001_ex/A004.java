67a6eef70a6a8c7ce455a6c80ea9ae9344e25748
shmoon2077 <margincall2077@icloud.com>
2025-02-17 17:57:26 +0900

    20250217

---
+++ shmooon/src/com/company/java001_ex/A004.java
@@ -0,0 +1,16 @@
+package com.company.java001_ex;
+
+public class A004 {
+	public static void main (String [] args) {
+		System.out.println ("1. 파일명 저장 : A004.java      ");
+		System.out.println ("2. 클래스 몇개 : 5개");
+		System.out.println ("3. 클래스 이름 : A004.class / A41.class / A42.class / A43.class / A43$44.class");
+	}
+
+}
+
+class A41{}
+class A42{}
+class A43{
+	class A44{}
+}
---
+++ shmooon/src/com/company/projeck1/java001/A001.java
@@ -0,0 +1,20 @@
+package com.company.projeck1.java001;
+
+public class A001 {
+	public static void main (String [] args) {
+		System.out.println("hello");
+		
+	} // end main
+
+} // end class
+
+// 한줄 주석 
+/*
+3줄 : public class A001 {} 
+     public  아무데서나 접근 가능 / class   부품 객체 / A001    객체 이름 /
+4줄 : JVM 구동 시작점
+     static 메모리상 사용가능 
+     void main (String [] args) 전원 버튼 이름
+5줄 : System.out.println("hello")
+     System : 운영 체제 out (콘솔창 cmd ) println (출력)
+*/
